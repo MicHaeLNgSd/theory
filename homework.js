24a2f8dacd42a382b08794490a4e7d8366431631
MicHaeLNgSd <koratgorovich2017@gmail.com>
2022-12-11 16:23:37 +0200

    filterByDateToday

--- homework.js
+++ homework.js
@@ -21,6 +21,13 @@ let cars = [
         "capacity": 2
     },
     {
+        "color": "yellow",
+        "type": "mini",
+        //"registration": new Date('2022-12-11').toLocaleDateString(), //today is 2022-12-11
+        "registration": new Date().toLocaleDateString(), // will update every day)
+        "capacity": 1
+    },
+    {
         "color": "white",
         "type": "sportcar",
         "registration": new Date('2022-10-06').toLocaleDateString(),
@@ -33,12 +40,16 @@ let cars = [
 
 //V1
 let filterByDateV1 = cars.filter((item) => item.registration.includes(new Date('2021-11-07').toLocaleDateString()))
-console.log('filterByDate', filterByDateV1);
+//console.log('filterByDate', filterByDateV1);
 
 //V2
 let filterByDateV2 = cars.filter((item) => item.registration.includes('08.12.2022'))
-console.log('filterByDate', filterByDateV2);
+//console.log('filterByDate', filterByDateV2);
 
 //спробуй тепер < заданої дати.
 let filterByDateEarlier = cars.filter((item) => item.registration < '08.12.2022')
-console.log('filterByDateEarlier', filterByDateEarlier);
+//console.log('filterByDateEarlier', filterByDateEarlier);
+
+//доопрацюй код, щоб знаходити записи по поточній даті
+let filterByDateToday = cars.filter((item) => item.registration == new Date().toLocaleDateString());
+console.log(filterByDateToday);
